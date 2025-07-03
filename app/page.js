3129cf2d660d212943a031dcc14a1c1fa80f70e5
daigo178 <daigo.178536@gmail.com>
2025-07-03 19:17:43 +0900

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,11 @@
+
+'use client';
+import ReservationForm from '../components/ReservationForm';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-4">
+      <ReservationForm />
+    </main>
+  );
+}
